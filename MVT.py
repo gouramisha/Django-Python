9fec9c46f80659714e72f2950ad4299faf63ac44
Amisha Gour <108326267+gouramisha@users.noreply.github.com>
2025-07-06 21:13:25 +0530

    Add files via upload

---
+++ MVT.py
@@ -0,0 +1,9 @@
+# ........MVT.............
+#  MVT Components:
+# 1. Model
+# Handles the database layer.
+
+# Defines the structure of your database using Python classes.
+
+# # Each model maps to a database table.
+
